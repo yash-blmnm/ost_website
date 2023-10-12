6a409367a2bf95ebe3e2d5c20488174b2ed35b09
Yasaswini <yasha.balu25@gmail.com>
2023-10-12 11:50:26 +1100

    contact form addition

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import ParseTextarea from "@/components/ParseTextArea";
 import { useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
 
 export default function page() {
   const {
@@ -12,49 +12,110 @@ export default function page() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  // const onSubmit =  (data) => console.log(data);
+  const onSubmit = (data) => {
+    const { EMAILJS_SERVICE_ID, EMAILJS_PUBLIC_API, EMAILJS_TEMPLATE_ID } =
+      process.env;
+    emailjs
+      .send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, data, EMAILJS_PUBLIC_API)
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    // e.target.reset();
+  };
 
   return (
     <section className="flex flex-col items-center justify-center m-12">
       <div className="text-3xl font-semibold">Contact Us</div>
       <form
-        className="flex flex-col lg:w-[50%] gap-4"
+        className="flex flex-col lg:w-[50%] gap-1"
         onSubmit={handleSubmit(onSubmit)}
       >
         <fieldset className="flex flex-col">
-          <label htmlFor="fullName">Full Name</label>
+          <label htmlFor="fullName">
+            Full Name
+            <sup className={`text-2xs ${errors.fullName && "text-red-600"}`}>
+              *
+            </sup>
+          </label>
           <input
             className="border border-gray-800 outline-none"
             {...register("fullName", { required: true })}
           />
-          {errors.exampleRequired && <span>This field is required</span>}
+          <span
+            className={`${
+              errors.fullName ? "text-red-600" : "invisible"
+            } text-xs`}
+          >
+            This field is required
+          </span>
         </fieldset>
 
         <fieldset className="flex flex-col">
-          <label htmlFor="email">Email</label>
+          <label htmlFor="replyTo">
+            Email
+            <sup className={`text-2xs ${errors.fullName && "text-red-600"}`}>
+              *
+            </sup>
+          </label>
           <input
             type="email"
             className="border border-gray-800 outline-none"
-            {...register("email", { required: true })}
+            {...register("replyTo", { required: true })}
           />
-          {errors.exampleRequired && <span>This field is required</span>}
+          <span
+            className={`${
+              errors.fullName ? "text-red-600" : "invisible"
+            } text-xs`}
+          >
+            This field is required
+          </span>
         </fieldset>
 
         <fieldset className="flex flex-col">
-          <label htmlFor="subject">Subject</label>
+          <label htmlFor="subject">
+            Subject
+            <sup className={`text-2xs ${errors.fullName && "text-red-600"}`}>
+              *
+            </sup>
+          </label>
           <input
             className="border border-gray-800 outline-none"
-            {...register("subject")}
+            {...register("subject", { required: true })}
           />
+          <span
+            className={`${
+              errors.fullName ? "text-red-600" : "invisible"
+            } text-xs`}
+          >
+            This field is required
+          </span>
         </fieldset>
 
         <fieldset className="flex flex-col">
-          <label htmlFor="message">Message</label>
-          {/* <textarea
-            className="border border-gray-800 outline-none"
-            {...register("message")}
-          /> */}
-          {/* <Controller name="emails" as={ParseTextarea} control={control} /> */}
+          <label htmlFor="message">
+            Message
+            <sup className={`text-2xs ${errors.fullName && "text-red-600"}`}>
+              *
+            </sup>
+          </label>
+          <textarea
+            rows={5}
+            className="border border-gray-800 outline-none resize-none"
+            {...register("message", { required: true })}
+          />
+          <span
+            className={`${
+              errors.fullName ? "text-red-600" : "invisible"
+            } text-xs`}
+          >
+            This field is required
+          </span>
         </fieldset>
 
         <input
